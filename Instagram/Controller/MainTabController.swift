2b398b672f4b85c352bdd24a791fb6deb8b07555
alex-andre-dev <alexandredmachado19@gmail.com>
2023-07-12 21:54:26 -0300

    Creating the main tabor controller and setting at the main view instead of the storyboard

---
+++ Instagram/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  Instagram
+//
+//  Created by Alexandre  Machado on 12/07/23.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
